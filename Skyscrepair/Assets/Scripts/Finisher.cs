d6a309e6506b34a3904da0b8ac3352b7dffdb146
David <davek032@outlook.com>
2020-02-02 13:07:23 +0000

    Firework & sign for completion

---
+++ Skyscrepair/Assets/Scripts/Finisher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finisher : MonoBehaviour
+{
+    public GameObject fireworks;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        fireworks.SetActive(true);
+    }
+}
